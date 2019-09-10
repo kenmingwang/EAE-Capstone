6d6806c7f49f4074014d546901dd9e8fd443445c
kenmingwang <fengfeng8867dd@hotmail.com>
2019-09-10 02:44:26 -0600

    Init

---
+++ Assets/Scripts/MoveCam.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    private Vector3 m_camRot;
+    private Transform m_camTransform;//摄像机Transform
+    private Transform m_transform;//摄像机父物体Transform
+    private Rigidbody rBody;
+    public float m_movSpeed = 10;//移动系数
+    public float m_rotateSpeed = 1;//旋转系数
+    private void Start()
+    {
+
+
+        m_camRot.x = 35;
+        m_camTransform = Camera.main.transform;
+
+        m_transform = GetComponent<Transform>();
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Control();
+        rBody.velocity = new Vector3(0, 0, 0);
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+    void Control()
+    {
+
+        if (Input.GetMouseButton(1))
+        {
+            //获取鼠标移动距离
+            float rh = Input.GetAxis("Mouse X");
+            float rv = Input.GetAxis("Mouse Y");
+
+            // 旋转摄像机
+            m_camRot.x -= rv * m_rotateSpeed;
+            m_camRot.y += rh * m_rotateSpeed;
+        }
+
+
+        m_camTransform.eulerAngles = m_camRot;
+
+        // 使主角的面向方向与摄像机一致
+
+
+        // 定义3个值控制移动
+        float xm = 0, ym = 0, zm = 0;
+
+        //按键盘W向上移动
+        if (Input.GetKey(KeyCode.W))
+        {
+            zm += m_movSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))//按键盘S向下移动
+        {
+            zm -= m_movSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))//按键盘A向左移动
+        {
+            xm -= m_movSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))//按键盘D向右移动
+        {
+            xm += m_movSpeed * Time.deltaTime;
+        }
+
+        m_transform.Translate(new Vector3(xm, 0, zm), Space.Self);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Enemy"))  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            rBody.velocity = Vector3.zero;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+    float moveSpeed;
+    float CurDist;
+    GameObject MainObject;
+    Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+
+        moveSpeed = 0.1f;
+        MainObject = GameObject.Find("MainObject");
+        Physics.IgnoreCollision(GetComponent<CapsuleCollider>(), MainObject.GetComponent<CapsuleCollider>());
+        Vector3 MainPos = MainObject.transform.position;
+        Vector3 CurPOs = transform.position;
+        CurDist = Vector3.Distance(MainPos, CurPOs);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 
+        Vector3 MainPos = MainObject.transform.position;
+        Vector3 CurPOs = transform.position;
+
+        float dist = Vector3.Distance(MainPos, CurPOs);
+
+        if (dist < CurDist && dist != 0 )
+        {
+            transform.LookAt(MainObject.transform);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            transform.position += transform.forward * moveSpeed;
+            MainPos = MainObject.transform.position;
+            CurPOs = transform.position;
+            dist = Vector3.Distance(MainPos, CurPOs);
+            CurDist = dist;
+            rBody.velocity = new Vector3(0,0,0);
+            return;
+        }
+
+        if (dist > CurDist && dist != 0)
+        {
+            transform.LookAt(MainObject.transform);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            transform.position += transform.forward * (moveSpeed/2);
+            MainPos = MainObject.transform.position;
+            CurPOs = transform.position;
+            dist = Vector3.Distance(MainPos, CurPOs);
+            CurDist = dist;
+            rBody.velocity = new Vector3(0, 0, 0);
+            return;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.name == "MainObject")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            rBody.velocity = Vector3.zero;
+        }
+    }
+}
